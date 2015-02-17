24316aa3f7f8c76d477246c125821ffd21ad0a51
Julien Cornebise <jucor@google.com>
2015-02-17 12:48:55 +0000

    Add Kolmogorov Smirnov one-sample test

--- luasrc/tests/testKSTest.lua
+++ luasrc/tests/testKSTest.lua
@@ -11,7 +11,7 @@ function mytest.testKSGaussian()
     local x, y  = torch.randn(nPoints), torch.randn(nPoints)
     x = x * sigma + mu
     y = y * sigma + mu
-    -- Chi-square test at 99%
+    -- KS test at 99%
     local p, d = distributions.kstwo(x, y)
     tester:assert(d, 'Test statistic should not be nil')
     tester:assert(p > 0.01, 'KS test rejects two samples from a same gaussian distribution')
@@ -21,6 +21,25 @@ function mytest.testKSGaussian()
     tester:assert(p < 0.01, 'KS test accepts two samples from different gaussian distributions')
 end
 
+
+function mytest.testKSOne()
+    local nPoints = 1000
+    local mu = 5
+    local sigma = 10
+    local x, y = torch.randn(nPoints)
+    x = x * sigma + mu
+    local function cdf(x)
+      return distributions.norm.cdf(x, mu, sigma)
+    end
+    -- KS test at 99%
+    local p, d = distributions.ksone(x, cdf)
+    tester:assert(d, 'Test statistic should not be nil')
+    tester:assert(p > 0.01, 'KS test rejects a sample from a gaussian distribution')
+    x = x + 2*mu
+    local p, d = distributions.ksone(x, cdf)
+    tester:assert(d, 'Test statistic should not be nil')
+    tester:assert(p < 0.01, 'KS test accepts a sample from a different gaussian distribution')
+end
 
 tester:add(mytest)
 return tester:run()
